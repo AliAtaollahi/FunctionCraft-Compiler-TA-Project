d33bcf3c9cc24e15640ac36a8140edefcb5e60f2
javad <javadkavian8@gmail.com>
2024-05-09 21:08:22 +0330

    update varItem

--- Phase3Template/src/main/symbolTable/item/VarItem.java
+++ Phase3Template/src/main/symbolTable/item/VarItem.java
@@ -1,12 +1,14 @@
 package main.symbolTable.item;
 
 import main.ast.nodes.expression.Identifier;
+import main.ast.type.Type;
 
 public class VarItem extends SymbolTableItem{
     public static String START_KEY = "VAR:";
     public VarItem(Identifier identifier){
         this.name = identifier.getName();
     }
+    public Type type;
     @Override
     public String getKey(){return START_KEY + this.name;}
 }
